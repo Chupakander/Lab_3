1e3cb08cc25f1e3e30d9c83411dc7a1a53ab364e
Chupa <matvey.savchenko.0202@mail.ru>
2021-12-14 22:36:45 +0300

    add Modification: "Exact value"

--- src/company/GornerTableModel.java
+++ src/company/GornerTableModel.java
@@ -1,68 +1,110 @@
 package company;
 
 import javax.swing.table.AbstractTableModel;
-@SuppressWarnings("serial")
-public class GornerTableModel extends AbstractTableModel {
-    private Double[] coefficients;
-    private Double from;
-    private Double to;
-    private Double step;
-    public GornerTableModel(Double from, Double to, Double step,
-
-                            Double[] coefficients) {
-
+//@SuppressWarnings("serial")
+public class GornerTableModel extends AbstractTableModel
+{
+    private	Double[] coefficients;
+    private	Double from;
+    private	Double to;
+    private	Double step;
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients)
+    {
         this.from = from;
         this.to = to;
         this.step = step;
         this.coefficients = coefficients;
     }
-    public Double getFrom() {
+    public Double getFrom()
+    {
         return from;
     }
-    public Double getTo() {
+    public Double getTo()
+    {
         return to;
     }
-    public Double getStep() {
+    public Double getStep()
+    {
         return step;
     }
-    public int getColumnCount() {
-// В данной модели два столбца
-        return 2;
+    public int getColumnCount()
+    {
+        // В данной модели два столбца
+        return 3;
     }
-    public int getRowCount() {
-// Вычислить количество точек между началом и концом отрезка
-// исходя из шага табулирования
+    public int getRowCount()
+    {
+        // Вычислить количество точек между началом и концом отрезка исходя из шага табулирования
         return new Double(Math.ceil((to-from)/step)).intValue()+1;
     }
-    public Object getValueAt(int row, int col) {
-// Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
+    public Object getValueAt(int row, int col)
+    {
+        // Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
         double x = from + step*row;
-        if (col==0) {
-// Если запрашивается значение 1-го столбца, то это X
+        if (col==0)
+        {
+            // Если запрашивается значение 1-го столбца, то это X
             return x;
-        } else {
-// Если запрашивается значение 2-го столбца, то это значение
-// многочлена
+        }
+        if(col==1)
+        {
+            // Если запрашивается значение 2-го столбца, то это значение многочлена
             Double result = 0.0;
-// Вычисление значения в точке по схеме Горнера.
-// Вспомнить 1-ый курс и реализовать
-// ...
-
+            for ( int i = coefficients.length-1; i >= 0; i-- )
+                result = coefficients[i]+result*x;
             return result;
         }
-    }
-    public String getColumnName(int col) {
-        switch (col) {
-            case 0:
-// Название 1-го столбца
-                return "Значение X";
-            default:
-// Название 2-го столбца
-                return "Значение многочлена";
+        else
+        {
+            Double result = 0.0;
+            double req=0.0;
+            int number;
+            int EndResult = 0;
+            for ( int i = coefficients.length-1; i >= 0; i-- )
+            {
+                result = coefficients[i]+result*x;
+            }
+            req=Math.abs(result%1);
+            if (req> 0 && req<1)
+                return EndResult == 1 ;
+            else
+                return EndResult == 0;
         }
     }
-    public Class<?> getColumnClass(int col) {
-// И в 1-ом и во 2-ом столбце находятся значения типа Double
-        return Double.class;
+    private void setText(String string) {
+        // TODO Auto-generated method stub
+
+    }
+    private int indexOf(char c) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    public String getColumnName(int col)
+    {
+        switch (col)
+        {
+            case 0:
+                // Название 1-го столбца
+                return "Значение X"	;
+
+            case 1:
+                return "Значение многочлена";
+
+            default:
+                return "Точное значение";
+        }
+    }
+    public Class<?> getColumnClass(int col)
+    {
+        switch (col)
+        {
+            case 0 :
+                return Double.class;
+            case 1 :
+                return Double.class;
+            default:
+                return Boolean.class ;
+
+        }
     }
 }
